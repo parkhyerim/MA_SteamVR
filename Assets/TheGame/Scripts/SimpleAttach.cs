3250837d1adad6f0773adea8628a0b7ba0d7a78c
Hyerim Park <hallohyerim@gmail.com>
2022-01-06 16:25:38 +0100

    Add interactable, event to steamVR scene

---
+++ Assets/TheGame/Scripts/SimpleAttach.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+
+public class SimpleAttach : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+
+    }
+
+
+}
