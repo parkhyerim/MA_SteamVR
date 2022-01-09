900f2cf21a331abf3fbe86f03d19157f4fb3e419
Hyerim Park <hallohyerim@gmail.com>
2022-01-09 14:46:59 +0100

    implement the Flip Card game

--- Assets/Game/Scripts/MemoryCard.cs
+++ Assets/Game/Scripts/MemoryCard.cs
@@ -1,10 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MemoryCard : MonoBehaviour
 {
+
+    //public XRSimpleInteractable simpleInteractable = null;
+
     public int identifier;
+    public float targetHeight = 10f;
+    public float targetRotation = 0;
+
+    private void Awake()
+    {
+       // simpleInteractable = GetComponent<XRSimpleInteractable>();
+    }
+
+    private void Update()
+    {
+        // Move up/down
+        //   float heightValue = Mathf.MoveTowards(transform.position.z, targetHeight, 1 * Time.deltaTime);
+        //   transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+
+        // Rotate
+        Quaternion rotationValue = Quaternion.Euler(0, targetRotation, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotationValue, 10 * Time.deltaTime);
+    }
+
+
+    private void OnSelectEnter()
+    {
+       // Debug.Log("selected");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void SelectCard()
+    {
+        Debug.Log("Clciked");
+        FindObjectOfType<MemoryCardGameManager>().CardClicked(this);
+    }
 
 
 }
--- Assets/Game/Scripts/MemoryCardGameManager.cs
+++ Assets/Game/Scripts/MemoryCardGameManager.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class MemoryCardGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public MemoryCard firstSelectedCard;
+    public MemoryCard secondSelectedCard;
+
+    private bool canClick = true;
+
+    public void CardClicked(MemoryCard card)
     {
-        
+        if (canClick == false || card == firstSelectedCard)
+        {
+            return;
+        }
+            
+        // Always rotate card front to show its image
+       // card.transform.localEulerAngles = new Vector3(0,0,0);
+        card.targetHeight = 0.05f;
+        card.targetRotation = 0;
+
+        if(firstSelectedCard == null)
+        {
+            firstSelectedCard = card;
+        }
+        else
+        {
+            // Second card selected;
+            secondSelectedCard = card;
+            canClick = false;
+            // 1 second later
+            Invoke("CheckMatch", 1);
+           
+        }
+       // Debug.Log(card.name + " " + card.identifier);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CheckMatch()
     {
-        
+      
+        // RESULT
+        if (firstSelectedCard.identifier == secondSelectedCard.identifier)
+        {
+            Destroy(firstSelectedCard.gameObject);
+            Destroy(secondSelectedCard.gameObject);
+        }
+        else
+        {
+            firstSelectedCard.targetRotation = 180;
+            secondSelectedCard.targetRotation = 180;
+
+
+        }
+
+        // RESET
+        firstSelectedCard = null;
+        secondSelectedCard = null;
+
+        canClick = true;
     }
 }
