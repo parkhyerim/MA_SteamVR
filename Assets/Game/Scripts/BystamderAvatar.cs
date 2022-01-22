382ef6464cc39a3df6d6dbd587a164ef880a5626
Hyerim Park <hallohyerim@gmail.com>
2022-01-22 15:28:19 +0100

    position of the presence ++

--- Assets/Game/Scripts/BystamderAvatar.cs
+++ Assets/Game/Scripts/BystamderAvatar.cs
@@ -5,56 +5,83 @@ using UnityEngine.UI;
 
 public class BystamderAvatar : MonoBehaviour
 {
-
     public GameObject bystanderTracker;
     public RawImage bystandreImage;
+    public RawImage frontImage;
+    public RawImage backImage;
     public bool sitLeft;
 
-    float rotationEulerY;
+    private float rotationEulerY;
+    private float RotationOffset = 0;
 
-    float yRotation;
-    float xRotation;
-    float zRotation;
-    Quaternion bystanderRotation;
-    Quaternion myRotation;
-    Vector3 newRotation;
-    Vector3 mirroredRotation;
+    private Quaternion bystanderRotation;
+    private Quaternion myRotation;
+    private Vector3 newRotation;
+    private Vector3 mirroredRotation;
+
+    private Vector3 cameraPosition;
 
     public Transform infoBubble;
     private Text infoText;
-    
-   // public GameObject mirrorAvatar;
+
+    // public GameObject mirrorAvatar;
+
+    /*
+     * awareness information: presence, (location), orientation 
+     */
+
+    private void Awake()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        frontImage.enabled = false;
+        backImage.enabled = false;
+       // frontImage.gameObject.SetActive(false);
+       // backImage.gameObject.SetActive(false);
         //eulerY = bystanderTracker.transform.eulerAngles.y;
         newRotation = new Vector3(0, 0, 0);
-
-        
+      
         if(infoBubble != null)
         {
             infoText = GetComponentInChildren<Text>();
-            infoText.text = "Bystander direction Info.";
+            infoText.text = "Bystander Rotation:";
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+
         rotationEulerY = bystanderTracker.transform.eulerAngles.y;
         //Debug.Log(eulerY);
         // transform.localEulerAngles = new Vector3(0, -1 * eulerY, 0);
+        transform.position = bystanderTracker.transform.position;
 
+        
+        
+        // The bystander is sitting to the left of the VR Player.
         if (sitLeft)
         {
+            // The bystander is turning to the right
             if (rotationEulerY > 30 && rotationEulerY < 110)
             {
               //  Debug.Log("The bystander is turning to the right \n at a " + rotationEulerY + "-degree angle.");
                 transform.localEulerAngles = new Vector3(0, 0, 0);
+               
 
                 // fully fade in (1) the image with the duration of 2
-                bystandreImage.CrossFadeAlpha(1, 1.0f, false);
+                //  bystandreImage.CrossFadeAlpha(1, 1.0f, false);
+
+                frontImage.enabled = true;
+                backImage.enabled = false;
+                // backImage.gameObject.SetActive(false);
+                // frontImage.gameObject.SetActive(true);
+
 
                 //if (infoBubble != null)
                 //    infoText.text = "Bystander's direnction: " + rotationEulerY;
@@ -63,7 +90,12 @@ public class BystamderAvatar : MonoBehaviour
             {
                // Debug.Log("The bystander is turning to the left at a " + rotationEulerY + "-degree angle.");
                 transform.localEulerAngles = new Vector3(0, 180, 0);
-                bystandreImage.CrossFadeAlpha(0, 1.0f, false);
+                //   bystandreImage.CrossFadeAlpha(0, 1.0f, false);
+
+                backImage.enabled = true;
+                frontImage.enabled = false;
+              //  backImage.gameObject.SetActive(true);
+              //  frontImage.gameObject.SetActive(false);
             }
         }
         else
@@ -74,8 +106,9 @@ public class BystamderAvatar : MonoBehaviour
                 transform.localEulerAngles = new Vector3(0, 0, 0);
 
                 // fully fade in (1) the image with the duration of 2
-                bystandreImage.CrossFadeAlpha(1, 1.0f, false);
-
+               // bystandreImage.CrossFadeAlpha(1, 1.0f, false);
+                backImage.gameObject.SetActive(false);
+                frontImage.gameObject.SetActive(true);
                 //if (infoBubble != null)
                 //    infoText.text = "Bystander's direnction: " + rotationEulerY;
             }
@@ -85,7 +118,9 @@ public class BystamderAvatar : MonoBehaviour
                // Debug.Log("The bystander is turning to the right at a " + rotationEulerY + "-degree angle.");
                 transform.localEulerAngles = new Vector3(0, 180, 0);
                 // fade out to nothing (0) the image with a duration of 2
-                bystandreImage.CrossFadeAlpha(0, 1.0f, false);
+              //  bystandreImage.CrossFadeAlpha(0, 1.0f, false);
+                backImage.gameObject.SetActive(true);
+                frontImage.gameObject.SetActive(false);
             }
         }
 
